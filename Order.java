0f9e0b42ad564a6b2eeeb8059423c7e86481aa87
Michelle D. Almen <117343266+learningmich@users.noreply.github.com>
2023-01-08 11:56:31 +0800

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,3 +1,34 @@
 public class Order
 {
+  private Customer owner;
+    private String order;
+
+    public Order(){
+        owner = null;
+        order = "unassigned";
+    }
+
+    public String getOrder () {
+        return order;
+    }
+
+    public String getCustomerName() {
+        return owner.getName();
+    }
+
+    public void setOrder (String customerOrder) {
+        order = customerOrder;
+    }
+
+    public void setCustomerName(Customer customer) {
+        owner = customer;
+    }
+
+
+
+    public String toString() {
+		return "Customer Name: " + owner.getName() + "\n"
+            + "Order: " + order;
+    }
+	
 }
